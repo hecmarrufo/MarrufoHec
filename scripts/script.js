9f60cf7a3b685a49bf750fe40a1647b1f917aaf0
marrufofinol@gmail.com <id19484615>
2018-03-13 03:29:26 -0300

    Version actualizada para tener correcto uso de databinding

--- scripts/script.js
+++ scripts/script.js
@@ -1,72 +1,46 @@
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope, $http) {
+    $http.get("./DataBases/data1.json").then(function (response) {
+        $scope.myData = response.data;
+    });
+});
 
-var requestURL = 'https://raw.githubusercontent.com/hecmarrufo/MarrufoHec/master/DataBases/data1.json';
-// base de datos 2, uncomment para utilizar
-// var requestURL = 'https://raw.githubusercontent.com/hecmarrufo/MarrufoHec/master/DataBases/data2.json';
-var request = new XMLHttpRequest();
-request.open('GET', requestURL);
-request.responseType = 'json';
-request.send();
-request.onload = function() {
-    var contenidos = request.response; //storing DataBases son variable
-//////////////////////////////////////////////////////////////////
-    for(i=0; i < contenidos.menu.length; i++) {
-        var elem = contenidos.menu[i];
-        var li = document.createElement('li');
-        li.innerHTML = elem;
-        document.getElementById('sidebar').appendChild(li);
-    }
-//////////////////////////////////////////////////////////////////
-    for(i=0; i < contenidos.sections.length; i++) {
-        var elem = contenidos.sections[i].name;
-        var li = document.createElement('li');
-        li.classList.add("selectores");
-        li.id = i;
-        li.innerHTML = elem;
-        document.getElementById('section').appendChild(li);
-        for (j = 0; j < contenidos.sections[i].kpis.length; j++) {
-            document.getElementById("kapis").innerHTML +="<div name='kpi"+i+"' class='ka' style='display: none'>"+
-                contenidos.sections[i].kpis[j].name + "<br><div class='nums'>" +
-                contenidos.sections[i].kpis[j].value +
-                " <span id='i"+i+"j"+j+"'> "+contenidos.sections[i].kpis[j].limit +
-                contenidos.sections[i].kpis[j].unit + "</span></div></div>";
-            var lim = parseInt(contenidos.sections[i].kpis[j].limit);
-            var val = parseInt(contenidos.sections[i].kpis[j].value);
-            if (val>lim){
-                document.getElementById("i"+i+"j"+j).style.color = "red";
-            }else {
-                document.getElementById("i"+i+"j"+j).style.color = "green";
-            }
-        }
-    }
-};
-//////////////////////////////////////////////////////////////////
-//show KPIS functionality
 function hasClass(elem, className) {
     return elem.className.split(' ').indexOf(className) > -1;
 }
 document.addEventListener('click', function (e) {
-    if (hasClass(e.target, 'selectores')) {
+
+    if (hasClass(e.target, 'tabs')) {
         var x = event.target.id;
         var hideThese = document.getElementsByClassName('ka');
         for (i = 0; i < hideThese.length; i++){
             hideThese[i].style.display = "none";
         }
-        document.getElementById(x).style.backgroundColor="lightgray";
-        document.getElementById(x).style.color="black";
-        var showThese = document.getElementsByName('kpi'+x);
-        document.getElementsByClassName('floating')[0].id=('kpi'+x);
+        var showThese = document.getElementsByName(x);
+        document.getElementsByClassName('floating')[0].id=(x);
         for (i = 0; i < showThese.length; i++) {
             showThese[i].style.display = "block";
         }
     }
-    if (hasClass(e.target, 'floating')) {
+
+        if (hasClass(e.target, 'floating')) {
         var x = event.target.id;
         var a = Math.floor(Math.random() * 10) + 1;
         var b = Math.floor(Math.random() * 10) + 1;
         if (a>b) {
-            document.getElementById("kapis").innerHTML += "<div name='" + x + "' class='ka'>Dummy Values<br><div class='nums'>" + a + " <span style='color: red'> " + b + "%</span></div></div>";
+            var newEle = "<div class='ka' name='"+x+"'>Dummy Values<br><div class='nums'>" + a + " <span style='color: red'> " + b + "%</span></div></div>";
+            angular.element(document.getElementById("kapis")).append(newEle)
         }else{
-            document.getElementById("kapis").innerHTML += "<div name='" + x + "' class='ka'>Dummy Values<br><div class='nums'>" + a + " <span style='color: green'> " + b + "%</span></div></div>";
+            var newEle = "<div class='ka' name='"+x+"'>Dummy Values<br><div class='nums'>" + a + " <span style='color: green'> " + b + "%</span></div></div>";
+            angular.element(document.getElementById("kapis")).append(newEle)
+        }
+        var showThese = document.getElementsByName(x);
+        for (i = 0; i < showThese.length; i++) {
+            showThese[i].style.display = "block";
         }
     }
 }, false);
+
+
+// var newEle = angular.element('<div class="my-blur"></div>');
+// angular.element(document.getElementById("kapis")).append(newEle)
